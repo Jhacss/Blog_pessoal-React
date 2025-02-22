98024a18bdb55b8c14b4d42bde138b2f14f782c8
Jhacss <henryki09.joao@gmail.com>
2025-02-22 20:40:57 -0300

    :sparkles: implementação página perfil

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,18 @@
 import { GithubLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { ReactNode, useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
-    return (
-        <>
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
             <div className="mt-auto flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
@@ -22,6 +29,11 @@ function Footer() {
                     </div>
                 </div>
             </div>
+       )
+    }
+    return (
+        <>
+            { component }
         </>
     )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,27 @@
+import { ReactNode, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../contexts/AuthContext"
-import { useContext } from "react"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
-    const { handleLogout } = useContext(AuthContext)
+    const { usuario, handleLogout } = useContext(AuthContext)
 
-    function logout(){
-        handleLogout
-        alert('O Usuário foi desconectado com sucesso! ')
-        navigate ('/')
+    function logout() {
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
     }
+    
+    let component: ReactNode
 
+    if (usuario.token !== "") {
 
-  return (
-      <>
-          <div className='w-full flex justify-center py-4
+        component = (
+
+                <div className='w-full flex justify-center py-4
                    bg-indigo-900 text-white'>
           
               <div className="container flex justify-between text-lg">
@@ -30,15 +34,22 @@ function Navbar() {
                       <Link to='/postagens' className='hover:underline'>Postagens</Link>
                       <Link to='/temas' className='hover:underline'>Temas</Link>
                       <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
-                      Perfil
+                      <Link to='/perfil' className='hover:underline'>Perfil</Link>
                       <Link to= '' onClick={logout} className = 'hover:underline'> 
                    Sair
                   </Link>
                   </div>
               </div>
           </div>
-      </>
-  )
+      )
+
+    }
+
+    return (
+        <>
+            { component }
+        </>
+    )
 }
 
 export default Navbar
